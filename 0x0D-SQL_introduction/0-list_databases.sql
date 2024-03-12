e2420480aa03174267874433a131085e382fc53c
YoussefLizdihar <youssefyd.3d@gmail.com>
2024-03-12 15:46:57 -0700

    {up 0}

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,5 +1,2 @@
--- 3 first students in the Batch ID=3
--- because Batch 3 is the best!
-SELECT id, name FROM students WHERE batch_id = 3 ORDER BY created_at DESC LIMIT 3;
-
+-- a script that lists all databases of your MySQL server
 SHOW DATABASES;
