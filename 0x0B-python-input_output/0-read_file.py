600cbf77f746c16cd12fdee56501c259ff2f3e5d
YoussefLizdihar <youssefyd.3d@gmail.com>
2024-05-28 17:35:02 -0700

    {update 0}

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ a function that reads a text file (UTF8) and prints it to stdout
 
 def read_file(filename=""):
     """func that reads a text file"""
-    with open('filename', 'r') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
         print(content)
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -1,1 +1,2 @@
 Create 0x0B-python-input_output
+create 0-read_file.py
