2665985beb13d7bb929850867b7a6217e878019f
YoussefLizdihar <youssefyd.3d@gmail.com>
2024-05-28 17:36:21 -0700

    {update 0}

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -8,4 +8,4 @@ def read_file(filename=""):
     """func that reads a text file"""
     with open(filename, 'r', encoding='utf-8') as file:
         content = file.read()
-        print(content)
+        print(content, end="")
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -1,2 +1,3 @@
 Create 0x0B-python-input_output
 create 0-read_file.py
+update 0
