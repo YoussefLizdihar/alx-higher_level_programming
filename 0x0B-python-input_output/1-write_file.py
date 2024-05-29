8aee624dafdb5cd4f61dc3052b76b4c6670323fb
YoussefLizdihar <youssefyd.3d@gmail.com>
2024-05-29 12:31:00 -0700

    {up 1}

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -8,5 +8,5 @@ and returns the number of characters written
 def write_file(filename="", text=""):
     """func that write a string to a text file"""
     with open(filename, 'w', encoding='utf-8') as file:
-        content = file.wtite(text)
+        content = file.write(text)
         return (content, end="")
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -2,3 +2,4 @@ Create 0x0B-python-input_output
 create 0-read_file.py
 update 0
 update 0
+create 1
