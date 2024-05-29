344a4f455d93c9cf4945b0e15fe88069c00041fd
YoussefLizdihar <youssefyd.3d@gmail.com>
2024-05-29 12:24:27 -0700

    {create 1}

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+a function that write a string into a text file (UTF8)
+and returns the number of characters written
+"""
+
+
+def write_file(filename="", text=""):
+    """func that write a string to a text file"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        content = file.wtite(text)
+        return (content, end="")
--- 0x0B-python-input_output/README.md
+++ 0x0B-python-input_output/README.md
@@ -1,3 +1,4 @@
 Create 0x0B-python-input_output
 create 0-read_file.py
 update 0
+update 0
